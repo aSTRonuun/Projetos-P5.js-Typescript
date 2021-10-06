b9b0713ad0693edb4149b75b4d62ac2257e1eba0
aSTRonuun <vitormanoel00710@gmail.com>
2021-10-06 15:30:21 -0300

    projeto snake atualizado

--- src/Projeto - Snake/sketch/sketch.ts
+++ src/Projeto - Snake/sketch/sketch.ts
@@ -5,8 +5,11 @@ const NC:number = 10;
 const LADO:number = 50;
 let snake_x:number = 4;
 let snake_y:number = 1;
+let snake_vx:number = 0;
+let snake_vy:number = 0;
 let snake_color: p5.Color;
 let cell_color: p5.Color;
+let timer:number = 0;
 
 function draw_cel(x:number, y:number, color: p5.Color){
   noStroke();
@@ -35,27 +38,44 @@ function snake_limit_loop() {
 }
 
 function keyPressed() {
-  if(keyCode === LEFT_ARROW)
-    snake_x -= 1; 
-  else if(keyCode === RIGHT_ARROW)
-    snake_x += 1;
-  else if(keyCode === UP_ARROW)
-    snake_y -= 1;
-  else if(keyCode === DOWN_ARROW)
-    snake_y += 1;
+  if(keyCode === LEFT_ARROW){
+    snake_vx = -1;
+    snake_vy = 0; 
+  }else if(keyCode === RIGHT_ARROW){
+    snake_vx = 1;
+    snake_vy = 0;
+  }else if(keyCode === UP_ARROW){
+    snake_vx = 0;
+    snake_vy = -1;
+  }else if(keyCode === DOWN_ARROW){
+    snake_vx = 0;
+    snake_vy = 1;
+  }
 }
 
+function snake_walk(){
+  if(frameCount - timer > 10) {
+    timer = frameCount;
+    snake_x += snake_vx;
+    snake_y += snake_vy;
+  }
+}
 
 function setup() {
 
   createCanvas(NC * LADO, NL * LADO);
-
+  frameRate(30);
   snake_color = color("pink");
   cell_color = color("gray");
 }
 
 function draw() {
+  snake_walk();
   snake_limit_loop();
   draw_mat(cell_color);
   draw_cel(snake_x, snake_y, snake_color);
+
+  fill(0);
+  textSize(25);
+  text(frameCount, 10, 30);
 }
