cfb647fe19578e48889119753194851f35bae4be
Vitor Alves <vitormanoel00710@gmail.com>
2022-06-26 20:21:56 -0300

    update game socket

--- src/Projeto - Flappy Bird/server/src/server.ts
+++ src/Projeto - Flappy Bird/server/src/server.ts
@@ -4,6 +4,8 @@ import http from "http";
 import path from "path";
 
 const users = []
+const team1 = []
+const team2 = []
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -11,16 +13,57 @@ const io = new socketio.Server(httpServer);
 
 app.use(express.static(path.resolve(__dirname, '../../')))
 
-io.on("connection", (socket) => {
-    console.log(`New connection: ${socket.id}`)
-
-    users.push(socket.id);
-
-    if(users.length === 4) {
-        console.log("partida com todos os players");
-        socket.broadcast.emit("start");
-        socket.emit("start")
+function sepateTeam(team1, team2) {
+    users.sort();
+    for(let i = 0; i < 2; i++) {
+        team1.push(users[i].id);
     }
+    for(let i = 2; i < 4; i++) {
+        team2.push(users[i].id);
+    }
+}
+
+function handleLose() {
+    if(team1.length == 0) {
+        console.log("team1 perdeu");
+    }else {
+        console.log("time2 perdeu");
+    }
+}
+
+io.on("connection", (socket) => {
+
+    users.push(socket);
+
+    if(users.length !== 4) {
+        console.log(`Esperando outros jogadores ${users.length}/2}`)
+    } else if (users.length === 4) {
+        console.log(`Esperando outros jogadores ${users.length}/2}`)
+        console.log("jogo iniciado")
+        sepateTeam(team1, team2);
+        io.emit("start");
+        
+    }
+    socket.on("hit", (id) => {
+        let index = team1.indexOf(id);
+        if(index > -1) {
+            team1.splice(index, 1);
+        }
+        index = team2.indexOf(id);
+        if(index > -1) {
+            team2.splice(index, 1);
+        }
+        handleLose();
+    })
+ 
+    socket.broadcast
+
+    for(let user of users) {
+        user.broadcast.emit("newPlayer")
+    }
+
+    
 })
+
 
 httpServer.listen(3333);
